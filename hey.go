3b3bd7ae61d40678b1031f5953fa339e27044bec
Doni Nurramdan <nurramdandoni@gmail.com>
2023-10-25 16:37:14 +0700

    membuat route sederhana

--- hey.go
+++ hey.go
@@ -1,13 +1,17 @@
 package main
 
-import (
-	"api-go/resource"
-	"fmt"
-)
+import "github.com/gofiber/fiber/v2"
 
 func main() {
-	fmt.Println("Hello")
-	resource.Model()
-	resource.Cal(2, 2, "meter")
+	// Buat instance Fiber
+	app := fiber.New()
+
+	// Definisikan route
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, Fiber!")
+	})
+
+	// Menjalankan server di port 3000
+	app.Listen(":3000")
 
 }
